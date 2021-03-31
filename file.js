3484eaad1ec056e0554a1719ac13f6f165a72c2a
Liubov Minakova <msnakova7@gmail.com>
2021-03-31 04:13:27 +0200

    no message

--- file.js
+++ file.js
@@ -1,1 +1,36 @@
+let body = document.body;
+let string = window.location.search;
+let url = 'https://api.github.com/users/Liubovminakova';
 
+
+fetch(url)
+    .then(res => res.json())
+    .then(json => {
+        console.log(json.avatar_url);
+        console.log(json.name);
+        console.log(json.bio);
+        console.log(json.html_url);
+
+        let photo = new Image(200,200);
+        photo.src = json.avatar_url;
+        body.append(photo);
+
+        let name = document.createElement('div');
+        if (json.name != null) {
+            name.innerHTML = json.name;
+        } else {
+            name.innerHTML = 'Информация о пользователе недоступна';
+        }
+        body.append(name);
+        name.addEventListener("click", () => window.location = json.html_url);
+
+        let bio = document.createElement('div');
+        if (json.bio != null) {
+            bio.innerHTML = json.bio;
+        } else {
+            bio.innerHTML = 'Информация о пользователе недоступна';
+        }
+        body.append(bio);
+
+    })
+    .catch(err => alert('Информация о пользователе недоступна'));
